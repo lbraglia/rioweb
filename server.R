8cc07dc028f953cc7698394415229455e5063519
Luca Braglia <lbraglia@gmail.com>
2015-11-17 12:34:03 +0100

    tests

--- server.R
+++ server.R
@@ -1,17 +1,31 @@
 library(shiny)
 library(rio)
-    
+library(tools)
+
 shinyServer(
     function(input, output){
 
-        output$imported_input_file <- renderTable({
-
-            input_file <- input$file
-            
+        output$imported_input_file <- {
+            input_file <- input$infile
+            input_file_format <- tools::file_ext(input_file$name)
             if (is.null(input_file))
                 return(NULL)
             else
-                rio::import(input_file$datapath)
-        })
+                import(file = input_file$datapath,
+                       format = input_file_format)
+        }
+
+        output$downloadData <- downloadHandler(
+            filename = function() {
+                name <- basename(file_path_sans_ext(input$infile$name))
+                paste0(name, '.', input$output_format)
+            },
+            content = function(file) {
+                rio::export(x = output$imported_input_file,
+                            file = file,
+                            format = input$output_format)
+            }
+        )
+        
     }
 )
--- ui.R
+++ ui.R
@@ -1,26 +1,22 @@
 library(shiny)
 library(markdown)
 
-available_formats <- c('dta', 'sav', 'xlsx')
+available_formats <- c('', 'rda', 'dta', 'sav', 'xlsx')
 
 shinyUI(fluidPage(
     titlePanel("rioweb"),
     sidebarLayout(
         sidebarPanel(
-            fileInput('file', 'Choose File'),
-            ## tags$hr(),
+            fileInput('infile'),
             selectInput('output_format',
                         label = h3('Output format'),
-                        choices = available_formats)
-            ## tags$hr()
-        ),
-        mainPanel(
-            tableOutput('imported_input_file')
-        )
+                        choices = available_formats),
+            downloadButton('downloadData', 'Download')
+        )## ,
+        
+        ## mainPanel(tableOutput('imported_input_file') )
     ),
-    fluidRow(
-        includeMarkdown("NOTES.md")
-    )
+    fluidRow(includeMarkdown("NOTES.md") )
     
 ))
 
